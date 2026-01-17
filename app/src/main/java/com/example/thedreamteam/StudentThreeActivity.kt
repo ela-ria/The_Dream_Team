aae3777fad34f6c52b12acb9791c2f00923afebf
leigcsn-sys <jlgacusana@gmail.com>
2026-01-17 12:27:09 +0800

    Added new profile and update my screen (Student 3)

--- app/src/main/java/com/example/thedreamteam/StudentThreeActivity.kt
+++ app/src/main/java/com/example/thedreamteam/StudentThreeActivity.kt
@@ -1,4 +1,30 @@
 package com.example.thedreamteam
 
-class StudentThreeActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
+
+
+class StudentThreeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    StudentScreen(
+                        profileImageRes = R.drawable.student3,
+                        studentName = "John Lei Gacusana",
+                        bio = "Dangal greetings! I am an Information Technology student who spends most of my time learning, gaming, and drawing." +
+                                " I enjoy exploring technology, solving problems, and leveling up my knowledge.",
+                        onBackClick = { finish() }
+                    )
+                }
+            }
+        }
+    }
 }
