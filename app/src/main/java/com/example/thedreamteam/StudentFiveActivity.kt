5fec6320d92fc8f28193f3b8c86b0e7d91723545
Jay Em <jayem.mejia22@gmail.com>
2026-01-18 22:16:17 +0800

    for student5, adding profile image and information

--- app/src/main/java/com/example/thedreamteam/StudentFiveActivity.kt
+++ app/src/main/java/com/example/thedreamteam/StudentFiveActivity.kt
@@ -1,4 +1,29 @@
 package com.example.thedreamteam
 
-class StudentFiveActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
+
+class StudentFiveActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    StudentScreen(
+                        profileImageRes = R.drawable.student5,
+                        studentName = "Jay-Em G. Mejia",
+                        bio = "Dangal Greetings! I am a currently a student studying Information Technology" +
+                                " . My hobby are playing basketball, reading manga, and workout.",
+                        onBackClick = { finish() }
+                    )
+                }
+            }
+        }
+    }
 }
