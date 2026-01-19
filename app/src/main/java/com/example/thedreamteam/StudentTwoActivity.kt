09041c0b4c354f960294255f4631477fe47a2aa7
ria lovelace <manuelagonzales0305@gmail.com>
2026-01-19 10:29:39 +0800

    Added image and bio for Student2

--- app/src/main/java/com/example/thedreamteam/StudentTwoActivity.kt
+++ app/src/main/java/com/example/thedreamteam/StudentTwoActivity.kt
@@ -1,4 +1,28 @@
 package com.example.thedreamteam
 
-class StudentTwoActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
+
+class StudentTwoActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    StudentScreen(
+                        profileImageRes = R.drawable.student2,
+                        studentName = "Kim Aldrin D. Maurin",
+                        bio = "Hi, I am student from Pamantasan ng Cabuyao, taking Bachelor of Science in Information Technology , A curious and motivated person who enjoys learning new things",
+                        onBackClick = { finish() }
+                    )
+                }
+            }
+        }
+    }
 }
